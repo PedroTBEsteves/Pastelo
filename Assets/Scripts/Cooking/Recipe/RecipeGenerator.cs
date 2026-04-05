5ee12e849d7b4d4f4c42c384bd4680f4369ff92b
txera <gabrieltxera@gmail.com>
2026-04-05 13:04:25 -0300

    implementa limite de até 3 tipos de recheio no pastel

--- Assets/Scripts/Cooking/Recipe/RecipeGenerator.cs
+++ Assets/Scripts/Cooking/Recipe/RecipeGenerator.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class RecipeGenerator
 {
+    private const int MaxAdditionalTypesWithoutMain = 3;
+    private const int MaxAdditionalTypesWithMain = 2;
+
     private readonly int _minFillings;
     private readonly int _maxFillingsInclusive;
     private readonly float _noMainProbability;
@@ -36,31 +39,67 @@ public class RecipeGenerator
         var fillings = new Dictionary<Filling, int>();
         
         var fillingsCount = GetFillingsCount();
+        var hasMain = false;
 
-        if (WillHaveMain() && fillingsCount > 0)
+        if (WillHaveMain() && fillingsCount > 0 && _ingredientsStorage.Mains.Count > 0)
         {
             var main = _ingredientsStorage.Mains.GetRandomElement();
             var mainCount = GetMainsCount(fillingsCount);
             fillingsCount -= mainCount;
             fillings.Add(main, mainCount);
+            hasMain = true;
         }
-        
-        var availableFillings = new List<Filling>(_ingredientsStorage.Sides);
-        
-        for (var i = 0; i < fillingsCount; i++)
-        {
-            var side = availableFillings.GetRandomElement();
-            availableFillings.Add(side);
-            fillings[side] = fillings.GetValueOrDefault(side) + 1;
-        }
+
+        AddSides(fillings, fillingsCount, hasMain);
         
         return new Recipe(dough, fillings);
     }
-
+    
     private int GetFillingsCount() => Random.Range(_minFillings, _maxFillingsInclusive + 1);
     
     private bool WillHaveMain() => Random.value > _noMainProbability;
 
     private int GetMainsCount(int fillingsCount) =>
         Mathf.Max(1, Mathf.CeilToInt(Random.Range(0.0f, _maxMainPercentage * fillingsCount)));
+
+    private void AddSides(Dictionary<Filling, int> fillings, int sidesCount, bool hasMain)
+    {
+        if (sidesCount <= 0 || _ingredientsStorage.Sides.Count == 0)
+            return;
+
+        var maxAdditionalTypes = hasMain ? MaxAdditionalTypesWithMain : MaxAdditionalTypesWithoutMain;
+        var maxSelectableTypes = Mathf.Min(maxAdditionalTypes, sidesCount, _ingredientsStorage.Sides.Count);
+
+        if (maxSelectableTypes <= 0)
+            return;
+
+        var selectedTypesCount = Random.Range(1, maxSelectableTypes + 1);
+        var selectedSides = GetRandomUniqueSides(selectedTypesCount);
+
+        foreach (var side in selectedSides)
+            fillings[side] = 1;
+
+        var remainingSidesCount = sidesCount - selectedSides.Count;
+
+        for (var i = 0; i < remainingSidesCount; i++)
+        {
+            var side = selectedSides.GetRandomElement();
+            fillings[side] = fillings.GetValueOrDefault(side) + 1;
+        }
+    }
+
+    private List<Side> GetRandomUniqueSides(int count)
+    {
+        var availableSides = new List<Side>(_ingredientsStorage.Sides);
+        var selectedSides = new List<Side>(count);
+
+        for (var i = 0; i < count; i++)
+        {
+            var sideIndex = Random.Range(0, availableSides.Count);
+            selectedSides.Add(availableSides[sideIndex]);
+            availableSides.RemoveAt(sideIndex);
+        }
+
+        return selectedSides;
+    }
 }
