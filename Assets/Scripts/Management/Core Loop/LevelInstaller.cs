77aea8a051ece8706df652160ba34c0d9d149dfe
txera <gabrieltxera@gmail.com>
2026-04-16 12:04:25 -0300

    move objetos exclusivos de nivel para escopo de nivel

--- Assets/Scripts/Management/Core Loop/LevelInstaller.cs
+++ Assets/Scripts/Management/Core Loop/LevelInstaller.cs
@@ -3,8 +3,24 @@ using UnityEngine;
 
 public class LevelInstaller : MonoBehaviour, IInstaller
 {
+    [SerializeField]
+    private Recipe _tutorialRecipe;
+
     public void InstallBindings(ContainerBuilder containerBuilder)
     {
-        
+        var tutorialState = new GameplayTutorialState(_tutorialRecipe);
+
+        containerBuilder
+            .AddScoped(typeof(GameplayTutorialEvents))
+            .AddScoped(_ => tutorialState, typeof(GameplayTutorialState))
+            .AddScoped(typeof(TutorialTargetRegistry))
+            .AddScoped(typeof(GameplayInteractionGate))
+            .AddScoped(typeof(LevelMoneyManager))
+            .AddScoped(typeof(LevelFlowController))
+            .AddScoped(typeof(TimeController))
+            .AddSingleton(Resources.Load("Settings/Management/OrderLoopSettings"))
+            .AddScoped(typeof(OrderController), typeof(OrderController), typeof(ITickable))
+            .AddScoped(typeof(CustomerQueue), typeof(CustomerQueue), typeof(ITickable))
+            .AddScoped(typeof(GameplayTutorialController));
     }
 }
--- Assets/Scripts/Management/ManagementInstaller.cs
+++ Assets/Scripts/Management/ManagementInstaller.cs
@@ -3,28 +3,12 @@ using UnityEngine;
 
 public class ManagementInstaller : MonoBehaviour, IInstaller
 {
-    [SerializeField]
-    private Recipe _tutorialRecipe;
-    
     public void InstallBindings(ContainerBuilder containerBuilder)
     {
-        var state = new GameplayTutorialState(_tutorialRecipe);
-        
         containerBuilder
-            .AddScoped(typeof(GameplayTutorialEvents))
-            .AddScoped(_ => state, typeof(GameplayTutorialState))
-            .AddScoped(typeof(TutorialTargetRegistry))
-            .AddScoped(typeof(GameplayInteractionGate))
             .AddSingleton(Resources.Load("Settings/Management/IngredientsStorageSettings"))
-            .AddScoped(typeof(IngredientsStorage))
+            .AddSingleton(typeof(IngredientsStorage))
             .AddSingleton(Resources.Load("Settings/Management/MoneySettings"))
-            .AddScoped(typeof(MoneyManager))
-            .AddScoped(typeof(LevelMoneyManager))
-            .AddScoped(typeof(LevelFlowController))
-            .AddScoped(typeof(TimeController))
-            .AddSingleton(Resources.Load("Settings/Management/OrderLoopSettings"))
-            .AddScoped(typeof(OrderController), typeof(OrderController), typeof(ITickable))
-            .AddScoped(typeof(CustomerQueue), typeof(CustomerQueue), typeof(ITickable))
-            .AddScoped(typeof(GameplayTutorialController));
+            .AddSingleton(typeof(MoneyManager));
     }
 }
