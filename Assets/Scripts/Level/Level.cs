5b25f02e56991d5c665d668f11d7e6a67cbfb1ad
txera <gabrieltxera@gmail.com>
2026-04-15 23:24:50 -0300

    cria classe de definição de nivel

---
+++ Assets/Scripts/Level/Level.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Level", fileName = "Level")]
+public class Level : ScriptableObject
+{
+    [field: SerializeField]
+    public LocalizedString Name { get; private set; }
+
+    [field: SerializeField]
+    public LocalizedString Description { get; private set; }
+
+    [SerializeField]
+    private Customer[] _customers;
+    
+    [field: SerializeField]
+    public int CustomersToServe { get; private set; }
+
+    public IEnumerable<Customer> Customers => _customers;
+}
