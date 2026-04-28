51e9346845874ded542460d956d9ff2fc6fe1642
txera <gabrieltxera@gmail.com>
2026-04-28 20:28:01 -0300

    implementa limite de clientes esperando na fila

--- Assets/Scripts/Cooking/Objects/Frying/DraggableClosedPastel.cs
+++ Assets/Scripts/Cooking/Objects/Frying/DraggableClosedPastel.cs
@@ -139,7 +139,7 @@ public class DraggableClosedPastel : ValidatedMonoBehaviour
         _frying = frying;
         _rawSlider.gameObject.SetActive(_frying);
         _cookedSlider.gameObject.SetActive(_frying);
-        _spriteRenderer.sortingOrder = frying ? 2 : 4;
+        _spriteRenderer.sortingOrder = frying ? 2 : 9;
     }
     
     private void OnHeld(PointerEventData eventData)
