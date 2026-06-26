c4a8e56ebd22fde32e2a2f67c6c8e61ae29c9e0c
txera <gabrieltxera@gmail.com>
2026-06-26 19:07:13 -0300

    desabilita pause no inicio do jogo

--- Assets/Scripts/UI/Pause/PauseButton.cs
+++ Assets/Scripts/UI/Pause/PauseButton.cs
@@ -18,6 +18,7 @@ public class PauseButton : ValidatedMonoBehaviour
     private void Awake()
     {
         _button.onClick.AddListener(TogglePause);
+        _pausePanel.gameObject.SetActive(false);
     }
 
     private void TogglePause()
